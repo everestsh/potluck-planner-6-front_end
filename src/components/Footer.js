72f031509f0053342c8b15531f163840222bbca6
sooof <sooof@me.com>
2021-12-20 02:37:28 -0800

    installed reacter router

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import '../CSS/Footer.css';
+
+const Footer = () =>{
+
+    return(
+        <div className="footer-section">
+            <h1>Footer</h1>
+        </div>
+    );
+}
+
+export default Footer;
